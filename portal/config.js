1cc9e32dd3aaab0f4a91b68494e458a74401750b
Douglas141103 <douglasazevedosouza@gmail.com>
2026-06-16 13:43:27 -0300

    Update config.js

--- portal/config.js
+++ portal/config.js
@@ -1,5 +1,5 @@
 window.INTEGRO_SUPABASE = {
-  url: "https://vdfnmwuxmpalsyuytvt.supabase.co",
+  url: "https://kvdfnmwuxmpalsyuytwt.supabase.co",
   anonKey: "sb_publishable_GP076p73HPtaU7Q6EyK3Mg_5fYc7pAu",
   bucket: "partner-materials",
   siteUrl: "https://www.institutointegro.com.br"
